9362e68460044194b4a9f1525d91e15578c0d14e
László Kovács <atomantinator@gmail.com>
2025-09-23 09:30:12 +0200

    format fix

--- Sources/FeatherStorageDriverLocal/Driver/LocalStorageComponent.swift
+++ Sources/FeatherStorageDriverLocal/Driver/LocalStorageComponent.swift
@@ -9,8 +9,8 @@ import FeatherComponent
 import FeatherStorage
 import Foundation
 import NIO
-import _NIOFileSystem
 import NIOFoundationCompat
+import _NIOFileSystem
 
 @dynamicMemberLookup
 struct LocalStorageComponent {
